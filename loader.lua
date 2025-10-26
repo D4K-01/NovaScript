d389b7657a0349e2ef4a5189edfef29dbff7ba16
D4K_ <officieldk72@gmail.com>
2025-10-26 17:32:30 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,14 @@
+-- loader.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TonRepo/TonCheat/main/core/ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TonRepo/TonCheat/main/core/http.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TonRepo/TonCheat/main/core/config.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TonRepo/TonCheat/main/core/modules.lua"))()
 
+-- Lancer le cheat
+getgenv().Cheat = {}
+Cheat.Name = "Ton Cheat"
+Cheat.Version = "1.0"
+Cheat.Game = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+-- Démarrer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TonRepo/TonCheat/main/main.lua"))()
